1d5755a767fa96a2000f9c5ec079de84437d4d12
Juanse Gimenez <juansegimenez94@gmail.com>
2024-01-09 17:33:48 -0300

    update(model/provider): add model validations

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,5 @@
 class Provider < ApplicationRecord
+  validates :name, :nit, :contact_name, presence: true
+  validates :nit, length: { maximum: 11 }, format: { with: /\A\d{9}-\d?\z/, message: '9 digits - 1 optional digit' }
+  validates :contact_phone, length: { maximum: 10 }
 end

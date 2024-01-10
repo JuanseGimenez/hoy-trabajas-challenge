e51c268445b93454f0dc115c084d9f68856a6fce
Juanse Gimenez <juansegimenez94@gmail.com>
2024-01-10 16:09:30 -0300

    feat: add missing actions with logic to providers controller
      - add missing actions to controller
      - remove filter on provider resource on routes
      - add translations to controller message and future views

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,10 +1,19 @@
 class ProvidersController < ApplicationController
-  before_action :set_banks, only: %i[new create]
+  before_action :set_banks, only: %i[new create edit update]
+  before_action :set_provider, only: %i[show edit update destroy]
+
+  def index
+    @providers = Provider.includes(bank_account: :bank)
+  end
+
+  def show; end
 
   def new
     @provider = Provider.new
     @provider.build_bank_account
   end
+
+  def edit; end
 
   def create
     @provider = Provider.new(provider_params)
@@ -18,11 +27,33 @@ class ProvidersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @provider.update(provider_params)
+        format.html { redirect_to providers_path, notice: I18n.t('provider.message_updated') }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @provider.destroy
+
+    respond_to do |format|
+      format.html { redirect_to providers_path, notice: I18n.t('provider.message_destroyed') }
+    end
+  end
+
   private
 
   def provider_params
     params.require(:provider).permit(:name, :nit, :contact_name, :contact_phone,
                                      bank_account_attributes: %i[number bank_id])
+  end
+
+  def set_provider
+    @provider = Provider.find(params[:id])
   end
 
   def set_banks

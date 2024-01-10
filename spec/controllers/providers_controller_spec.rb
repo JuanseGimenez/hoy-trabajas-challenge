8aee436027c44d158e974762d8c7c38895baf171
Juanse Gimenez <juansegimenez94@gmail.com>
2024-01-10 16:08:28 -0300

    test(provider): add missing test to controller and routing [REQ-03]

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -1,4 +1,30 @@
 describe ProvidersController do
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get :index
+      expect(assigns(:providers)).to eq([])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    let(:provider) { create(:provider) }
+
+    it 'returns a success response' do
+      get :show, params: { id: provider.id }
+      expect(assigns(:provider)).to eq(provider)
+    end
+
+    it 'renders the show template' do
+      get :show, params: { id: provider.id }
+      expect(response).to render_template(:show)
+    end
+  end
+
   describe 'GET #new' do
     it 'returns a success response' do
       get :new
@@ -8,6 +34,20 @@ describe ProvidersController do
     it 'renders the new template' do
       get :new
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:provider) { create(:provider) }
+
+    it 'returns a success response' do
+      get :edit, params: { id: provider.id }
+      expect(assigns(:provider)).to eq(provider)
+    end
+
+    it 'renders the edit template' do
+      get :edit, params: { id: provider.id }
+      expect(response).to render_template(:edit)
     end
   end
 
@@ -75,6 +115,71 @@ describe ProvidersController do
         post :create, params: { provider: provider_params }
         expect(response).to render_template(:new)
       end
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:bank) { create(:bank) }
+    let(:provider) { create(:provider) }
+    let(:provider_params) do
+      {
+        name: 'Provider name',
+        nit: '123456789-1',
+        contact_name: 'Contact name',
+        contact_phone: '1234567890',
+        bank_account_attributes: {
+          number: '123456789012345',
+          bank_id: bank.id
+        }
+      }
+    end
+
+    context 'with valid params' do
+      it 'redirects to the provider' do
+        put :update, params: { id: provider.id, provider: provider_params }
+        expect(response).to redirect_to(providers_path)
+      end
+
+      it 'updates the requested provider' do
+        put :update, params: { id: provider.id, provider: provider_params }
+        provider.reload
+        expect(provider.name).to eq('Provider name')
+      end
+    end
+
+    context 'with invalid params' do
+      let(:provider_params) do
+        {
+          name: '',
+          nit: '',
+          contact_name: '',
+          contact_phone: '',
+          bank_account_attributes: {
+            number: '',
+            bank_id: ''
+          }
+        }
+      end
+
+      it 'renders the edit template' do
+        put :update, params: { id: provider.id, provider: provider_params }
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:provider) { create(:provider) }
+
+    it 'redirects to the providers list' do
+      delete :destroy, params: { id: provider.id }
+      expect(response).to redirect_to(providers_path)
+    end
+
+    it 'destroys the requested provider' do
+      expect do
+        delete :destroy, params: { id: provider.id }
+      end.to change(Provider, :count).by(-1)
     end
   end
 end
--- spec/routes/providers_routing_spec.rb
+++ spec/routes/providers_routing_spec.rb
@@ -1,4 +1,11 @@
 describe 'routing to providers', type: :routing do
+  it 'routes /providers to providers#index' do
+    expect(get: '/providers').to route_to(
+      controller: 'providers',
+      action: 'index'
+    )
+  end
+
   it 'routes /providers/new to providers#new' do
     expect(get: '/providers/new').to route_to(
       controller: 'providers',
@@ -10,6 +17,30 @@ describe 'routing to providers', type: :routing do
     expect(post: '/providers').to route_to(
       controller: 'providers',
       action: 'create'
+    )
+  end
+
+  it 'routes /providers/1/edit to providers#edit' do
+    expect(get: '/providers/1/edit').to route_to(
+      controller: 'providers',
+      action: 'edit',
+      id: '1'
+    )
+  end
+
+  it 'routes /providers/1 to providers#update' do
+    expect(put: '/providers/1').to route_to(
+      controller: 'providers',
+      action: 'update',
+      id: '1'
+    )
+  end
+
+  it 'routes /providers/1 to providers#destroy' do
+    expect(delete: '/providers/1').to route_to(
+      controller: 'providers',
+      action: 'destroy',
+      id: '1'
     )
   end
 end

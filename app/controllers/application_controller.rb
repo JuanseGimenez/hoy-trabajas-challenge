08f71860468553b15e662bbdf65b2185a1bb693f
Juanse Gimenez <juansegimenez94@gmail.com>
2024-01-11 22:56:52 -0300

    fix: add format_json? method on protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery if: -> { format_json? }
+  # TODO: Add authentication for JSON requests(JWT + Devise)
+  before_action :authenticate_user!, unless: :format_json?
+  rescue_from ActiveRecord::RecordNotFound, with: ->(e) { format_json? ? record_not_found : raise(e) }
+
+  private
+
+  def format_json? = request.format.json?
 end
